aa9a03f5a7c3f5b6b1d61c39c64f3b715f282142
SeyiOrafidiya <67023385+SeyiOrafidiya@users.noreply.github.com>
2021-04-12 22:32:41 +0100

    Update on css and js
    Formatted destination results and changed from prague to madrid

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 let mainContainer = document.getElementById("myData");
 let userContainer = document.getElementById("userinput");
 
-fetch("https://tequila-api.kiwi.com/v2/search?fly_from=LON&fly_to=PRG&date_from=01%2F08%2F2021&date_to=05%2F08%2F2021&return_from=03%2F09%2F2021&return_to=04%2F09%2F2021", {
+fetch("https://tequila-api.kiwi.com/v2/search?fly_from=LON&fly_to=MAD&date_from=01%2F08%2F2021&date_to=05%2F08%2F2021&return_from=03%2F09%2F2021&return_to=04%2F09%2F2021", {
 	"method": "GET",
 	"headers": {
         "accept": "application/json",
@@ -23,7 +23,7 @@ fetch("https://tequila-api.kiwi.com/v2/search?fly_from=LON&fly_to=PRG&date_from=
             let localDate = new Date(utcDate);
             let localDateReturn = new Date(utcDateReturn);
             let div = document.createElement("div");
-            div.innerHTML = `From: ${jsonResponse.data[i].cityFrom} To: ${jsonResponse.data[i].cityTo} Price per Adult: ${jsonResponse.data[i].fare.adults} Departure: ${localDate.toString()} Return: ${localDateReturn.toString()}`;
+            div.innerHTML = `From: ${jsonResponse.data[i].cityFrom} To: ${jsonResponse.data[i].cityTo} Price per Adult: ${jsonResponse.data[i].fare.adults} Departure: ${localDate.toString().replace(":00 GMT+0100 (British Summer Time)", "")} Return: ${localDateReturn.toString().replace(":00 GMT+0100 (British Summer Time)", "")}`;
             mainContainer.appendChild(div);
         }
     }
@@ -53,9 +53,9 @@ const getSuggestions = () => { //REDUCE THIS. user input place and gets aggregat
         for (let i = 0; i < 11; i++) {
             let ul = document.createElement("ul");
             let li = document.createElement("li");
-            ul.classList.add("list-group", "list-group-numbered");
+            ul.classList.add("list-group", "list-group-numbered", "capitalize");
             li.classList.add("list-group-item");
-            li.innerHTML = `${jsonResponse.locations[i].slug.replace("-", ", ")}`;
+            li.innerHTML = `${jsonResponse.locations[i].slug.replace("-", " ").replace("-", " ").replace("-", " ").replace("-", " ").replace("-", " ").replace("-", " ")}`;
             userContainer.appendChild(div);
             userContainer.appendChild(ul);
             ul.appendChild(li);
